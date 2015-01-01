42a8205f652a7366db59f2d7bb00acd238e02d66
JiangDapeng <asuwill.jdp@gmail.com>
2015-01-01 11:48:09 +0800

    complete f_a_b using look-up table to speed up

---
+++ task/learn-python/f_a_b.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#-*-coding=utf-8-*-
+
+def f_a_b(a,b):
+  '''
+  f_a_b(a,b) = 
+    0, if a<0 and b <0
+    1, else if a == 0 
+    a, else if b == 0
+    f_a_b(a-1,b) + 2* f_a_b(a,b-1) + 1, otherwise
+  '''
+  if a >= 0 and b >= 0:
+    cache = [[None for j in range(b+1)] for i in range(a+1)]
+    for j in range(b+1):
+      cache[0][j] = 1
+    for i in range(1,a+1):
+      cache[i][0] = i
+  def do_f(a,b):
+    #print("a = %d, b=%d" % (a,b))
+    if a < 0 or b <0:
+      return 0
+    if cache[a][b] == None:
+      cache[a][b] = do_f(a-1,b) + 2*do_f(a,b-1) + 1
+    return cache[a][b]
+  return do_f(a,b)
